40abce27926d20081ea189cbe4fc5f45241c8c42
kudzanai92 <keezentpty@gmai.com>
2023-08-24 08:38:31 -0700

    update

--- 4_add.c
+++ 4_add.c
@@ -1,185 +1,30 @@
 #include "monty.h"
 
 /**
- * f_add - add first 2 elements of the stack
- * @head: pointer to pointer to first node
- * @counter: line counter
- *
- * Return: none
- */
+* addnode - add node to top of stack
+* @head: first node
+* @n: new value
+*
+* Return: none
+*/
 
-void f_add(stack_t **head, unsigned int counter)
+void addnode(stack_t **head, int n)
 {
-	stack_t *h;
-	int length = 0, temp;
-
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	temp = h->n + h->next->n;
-	h->next->n = temp;
-	*head = h->next;
-	free(h);
-}
-
-/**
- * f_sub - subtract 2 elements of the stack
- * @head: pointer to pointer to first node
- * @counter: line counter
- *
- * Return: none
- */
-
-void f_sub(stack_t **head, unsigned int counter)
-{
-	stack_t *temp;
-	int sub, nd;
+	stack_t *new_node, *temp;
 
 	temp = *head;
-	for (nd = 0; temp != NULL; nd++)
-		temp = temp->next;
+	new_node = malloc(sizeof(stack_t));
 
-	if (nd < 2)
+	if (new_node == NULL)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
+		printf("Error\n");
+		exit(0);
 	}
-	temp = *head;
-	sub = temp->next->n - temp->n;
-	temp->next->n = sub;
-	*head = temp->next;
-	free(temp);
-}
+	if (temp)
+		temp->prev = new_node;
 
-/**
- * f_mul - multiply first 2 elements of the stack
- * @head: pointer to pointer to first node
- * @counter: line counter
- *
- * Return: none
- */
-
-void f_mul(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int length = 0, temp;
-
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	temp = h->next->n * h->n;
-	h->next->n = temp;
-	*head = h->next;
-	free(h);
-}
-
-/**
- * f_div - divide first 2 elements of the stack
- * @head: pointer to pointer to first node
- * @counter: line counter
- *
- * Return: none
- */
-
-void f_div(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int length = 0, temp;
-
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	temp = h->next->n / h->n;
-	h->next->n = temp;
-	*head = h->next;
-	free(h);
-}
-
-/**
- * f_mod - compute modulos of two elements in stack
- * @head: pointer to pointer to first node
- * @counter: line counter
- *
- * Return: none
- */
-
-void f_mod(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int length = 0, temp;
-
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	temp = h->next->n % h->n;
-	h->next->n = temp;
-	*head = h->next;
-	free(h);
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
 }
